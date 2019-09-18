024eac988938f7f60f493b4f58d5cf2b6f3186af
Matt <ymc006@ucsd.edu>
2019-09-18 08:27:56 -0700

    added basic code for  server.js, routes files, and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// ===============================================================================
+// DATA
+// The below data will hold all the information of the users who have submitted a survey.
+// ===============================================================================
+
+var friendArray = [];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
+  
